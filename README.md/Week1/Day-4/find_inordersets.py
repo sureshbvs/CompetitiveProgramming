c73f16228e40ed260b74ea3999451ea64b9baf7c
B.V.Suresh Reddy <31233316+sureshbvs@users.noreply.github.com>
2018-06-28 17:58:24 +0530

    Update find_inordersets.py

--- README.md/Week1/Day-4/find_inordersets.py
+++ README.md/Week1/Day-4/find_inordersets.py
@@ -1,1 +1,70 @@
+import unittest
 
+def contains(ordered_list, number):
+    n=len(ordered_list)
+    x=binarySearch(ordered_list,0,n-1,number)
+    return x
+    
+    
+def binarySearch(l,low,high,e):
+    
+    while low<=high:
+    
+        x=(high+low)//2
+       
+        
+        if l[x]>e:
+            high=x-1
+            
+       
+        elif l[x]<e:
+            low=x+1
+        
+        else:
+            return True
+    return False
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_empty_list(self):
+        result = contains([], 1)
+        self.assertFalse(result)
+
+    def test_one_item_list_number_present(self):
+        result = contains([1], 1)
+        self.assertTrue(result)
+
+    def test_one_item_list_number_absent(self):
+        result = contains([1], 2)
+        self.assertFalse(result)
+
+    def test_small_list_number_present(self):
+        result = contains([2, 4, 6], 4)
+        self.assertTrue(result)
+
+    def test_small_list_number_absent(self):
+        result = contains([2, 4, 6], 5)
+        self.assertFalse(result)
+
+    def test_large_list_number_present(self):
+        result = contains([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8)
+        self.assertTrue(result)
+
+    def test_large_list_number_absent(self):
+        result = contains([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0)
+        self.assertFalse(result)
+
+    def test_large_list_number_first(self):
+        result = contains([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1)
+        self.assertTrue(result)
+
+    def test_large_list_number_last(self):
+        result = contains([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10)
+        self.assertTrue(result)
+
+
+unittest.main(verbosity=2)

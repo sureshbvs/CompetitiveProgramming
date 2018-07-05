324ea575dc95168d502125ff9ddf3e9a332fadf1
B.V.Suresh Reddy <31233316+sureshbvs@users.noreply.github.com>
2018-07-05 15:06:48 +0530

    Update reversestringinplace.java

--- README.md/Week2/Day3/reversestringinplace.java
+++ README.md/Week2/Day3/reversestringinplace.java
@@ -1,1 +1,55 @@
+import java.util.Arrays;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
+import static org.junit.Assert.*;
+
+public class Solution {
+
+public static char[] reverse(char []arr) {
+  for(int i = 0, j = arr.length- 1; i < arr.length/2; i++, j--) {
+    char c = arr[i];
+    arr[i] = arr[j];
+    arr[j] = c;
+  }
+  return arr;
+}
+
+    // tests
+
+    @Test
+    public void emptyStringTest() {
+        final char[] expected = "".toCharArray();
+        final char[] actual = "".toCharArray();
+        reverse(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void singleCharacterStringTest() {
+        final char[] expected = "A".toCharArray();
+        final char[] actual = "A".toCharArray();
+        reverse(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void longerStringTest() {
+        final char[] expected = "EDCBA".toCharArray();
+        final char[] actual = "ABCDE".toCharArray();
+        reverse(actual);
+        assertArrayEquals(expected, actual);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}

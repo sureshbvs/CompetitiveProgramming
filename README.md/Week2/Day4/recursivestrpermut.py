3205b1c4d45d1a11b839fe0603305b6bb4668d32
B.V.Suresh Reddy <31233316+sureshbvs@users.noreply.github.com>
2018-07-06 11:33:51 +0530

    Update recursivestrpermut.py

--- README.md/Week2/Day4/recursivestrpermut.py
+++ README.md/Week2/Day4/recursivestrpermut.py
@@ -1,1 +1,40 @@
+import unittest
+def get_permutations(string):
+    if len(string) <= 1:
+        return set([string])
+    all_chars_except_last = string[:-1]
+    last_char = string[-1]
+    permutations_of_all_chars_except_last = get_permutations(all_chars_except_last)
+    permutations = set()
+    for permutation_of_all_chars_except_last in permutations_of_all_chars_except_last:
+        for position in range(len(all_chars_except_last) + 1):
+            permutation = (permutation_of_all_chars_except_last[:position] + last_char
+                + permutation_of_all_chars_except_last[position:])
+            permutations.add(permutation)
+    return permutations
+# Tests
 
+class Test(unittest.TestCase):
+
+    def test_empty_string(self):
+        actual = get_permutations('')
+        expected = set([''])
+        self.assertEqual(actual, expected)
+
+    def test_one_character_string(self):
+        actual = get_permutations('a')
+        expected = set(['a'])
+        self.assertEqual(actual, expected)
+
+    def test_two_character_string(self):
+        actual = get_permutations('ab')
+        expected = set(['ab', 'ba'])
+        self.assertEqual(actual, expected)
+
+    def test_three_character_string(self):
+        actual = get_permutations('abc')
+        expected = set(['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)

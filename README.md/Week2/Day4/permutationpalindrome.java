90cc01c292fa6e0424845ee309eb4e6a4be39334
B.V.Suresh Reddy <31233316+sureshbvs@users.noreply.github.com>
2018-07-06 11:38:28 +0530

    Update permutationpalindrome.java

--- README.md/Week2/Day4/permutationpalindrome.java
+++ README.md/Week2/Day4/permutationpalindrome.java
@@ -1,1 +1,73 @@
+//O(n)
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+
+public class Solution {
+    public static boolean hasPalindromePermutation(String str) { {
+        int[] array = new int[128];
+        for (int i = 0; i < str.length(); i++) {
+            array[str.charAt(i)]++;
+        }
+        int count = 0;
+        for (int j = 0; j < array.length && count <= 1; j++) {
+            count += array[j] % 2;
+        }
+        if (count > 1)
+            return false;
+    }
+ return true;
+    }
+
+    // tests
+
+    @Test
+    public void permutationWithOddNumberOfCharsTest() {
+        final boolean result = hasPalindromePermutation("aabcbcd");
+        assertTrue(result);
+    }
+
+    @Test
+    public void permutationWithEvenNumberOfCharsTest() {
+        final boolean result = hasPalindromePermutation("aabccbdd");
+        assertTrue(result);
+    }
+
+    @Test
+    public void noPermutationWithOddNumberOfChars() {
+        final boolean result = hasPalindromePermutation("aabcd");
+        assertFalse(result);
+    }
+
+    @Test
+    public void noPermutationWithEvenNumberOfCharsTest() {
+        final boolean result = hasPalindromePermutation("aabbcd");
+        assertFalse(result);
+    }
+
+    @Test
+    public void emptyStringTest() {
+        final boolean result = hasPalindromePermutation("");
+        assertTrue(result);
+    }
+
+    @Test
+    public void oneCharacterStringTest() {
+        final boolean result = hasPalindromePermutation("a");
+        assertTrue(result);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
 

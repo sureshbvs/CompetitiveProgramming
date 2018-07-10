d8eb3a3751da3eac6ad1c84d0b1ef38b1a9bb414
B.V.Suresh Reddy <31233316+sureshbvs@users.noreply.github.com>
2018-07-10 15:44:36 +0530

    Create simulate5.java

---
+++ README.md/Week3/Day1/simulate5.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+class Solution {
+    private static final Random rnd = new Random();
+
+    private static int rand7() {
+        return rnd.nextInt(7) + 1;
+    }
+    public static int rand5() {
+    return (rand7() > 5) ? rand5():rand7();
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println( rand5());
+        }
+        System.out.println();
+    }
+}

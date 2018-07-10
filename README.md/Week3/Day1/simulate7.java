ccbbbd1202e30065f7cbc8f2a287cf120cab61f4
B.V.Suresh Reddy <31233316+sureshbvs@users.noreply.github.com>
2018-07-10 15:54:47 +0530

    Update simulate7.java

--- README.md/Week3/Day1/simulate7.java
+++ README.md/Week3/Day1/simulate7.java
@@ -1,1 +1,19 @@
-
+import java.util.Random;
+class Solution {
+    private static final Random rnd = new Random();
+    private static int rand5() {
+        return rnd.nextInt(5) + 1;
+    }
+    public static int rand7() {
+        if( rand5()+rand5()*5 - 6<21)
+            return ((rand5()+rand5()*5 - 6)%7)+1;
+        else
+            return rand7();
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < 14; i++) {
+            System.out.printf("%d ", rand7());
+        }
+        System.out.println();
+    }
+}

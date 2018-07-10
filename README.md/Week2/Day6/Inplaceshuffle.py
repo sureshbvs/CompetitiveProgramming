4bad9545fef58c68bf7f6b1a7169411920aaa669
B.V.Suresh Reddy <31233316+sureshbvs@users.noreply.github.com>
2018-07-10 14:55:27 +0530

    Update Inplaceshuffle.py

--- README.md/Week2/Day6/Inplaceshuffle.py
+++ README.md/Week2/Day6/Inplaceshuffle.py
@@ -1,1 +1,12 @@
-
+import random
+def get_random(floor, ceil):
+    return random.randrange(floor, ceil + 1)
+def shuffle(list):
+    for i in range(0, len(list) - 1):
+        j = get_random(0, len(list) - 1)
+        if j != i:
+            list[i], list[j] = list[j], list[i]
+sample_list = [1, 2, 3, 4, 5]
+print ('Shuffling sample list...')
+shuffle(sample_list)
+print (sample_list)
